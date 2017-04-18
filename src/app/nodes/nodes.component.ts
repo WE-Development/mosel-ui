03f92e458dc6d570272247e6ba013fc2ea95db95
robin <engel.robin@gmx.de>
2017-04-18 21:35:09 +0200

    cleanup

--- src/app/nodes/nodes.component.ts
+++ src/app/nodes/nodes.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
 import {InfoService} from "../rest-service/info.service";
 import {InfoResponse} from "../rest-service/info-response";
-import {MdSnackBar} from "@angular/material";
 import {HttpUtils} from "../http-utils";
 
 @Component({
@@ -13,9 +11,7 @@ import {HttpUtils} from "../http-utils";
 })
 export class NodesComponent implements OnInit {
 
-  constructor(private router: Router,
-              private infoService: InfoService,
-              private snackBar: MdSnackBar,
+  constructor(private infoService: InfoService,
               private httpUtils: HttpUtils) {
   }
 
