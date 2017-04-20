75d40e1689d340f31f49a2390016a833cfbb651a
robin <engel.robin@gmx.de>
2017-04-20 18:51:45 +0200

    rearrange imports

--- src/app/nodes/nodes.module.ts
+++ src/app/nodes/nodes.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
-import {NodesComponent} from "./nodes.component";
-import {NodeDetailComponent} from "./node-detail/node-detail.component";
-import {NodesRoutingModule} from "./nodes-routing.module";
-import {MdButtonModule, MdListModule, MdSidenavModule} from "@angular/material";
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpModule} from "@angular/http";
 import {BrowserModule} from "@angular/platform-browser";
+import {NodesComponent} from "./nodes.component";
+import {NodeDetailComponent} from "./node-detail/node-detail.component";
+import {NodesRoutingModule} from "./nodes-routing.module";
+import {MdButtonModule, MdListModule, MdSidenavModule} from "@angular/material";
 
 @NgModule({
   imports: [
